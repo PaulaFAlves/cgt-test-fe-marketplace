5d6150e367e2482afd8aa682170cf8c74c165a4a
paulafalves <paulafernandaalves@icloud.com>
2022-09-11 15:43:30 -0300

    feat: create layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,26 @@
+import React, { useContext, useEffect, useState } from "react";
+import Context from "../../Context";
 import { Outlet, Link } from "react-router-dom";
 
 import { List } from "./styles";
+import Badge from "../Badge";
 
 const Layout = () => {
+  const countContext = useContext(Context);
+  const { count } = countContext;
+  const [cartHasItems, setCartHasItems] = useState(false);
+  const [countItemsQuantity, setCountItemsQuantity] = useState(false);
+
+  useEffect(() => {
+    const quantity = Object.values(count).reduce(
+      (t, { quantity }) => t + quantity,
+      0
+    );
+
+    setCountItemsQuantity(quantity);
+    setCartHasItems(quantity > 0);
+  }, [count]);
+
   return (
     <>
       <nav>
@@ -11,10 +29,12 @@ const Layout = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/cart">Cart</Link>
+            <Link to="/products">Products</Link>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <Link to="/cart">
+              Cart{cartHasItems && <Badge quantity={countItemsQuantity} />}
+            </Link>
           </li>
         </List>
       </nav>
