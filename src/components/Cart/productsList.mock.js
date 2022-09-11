6d64e8f3397615df727f242eba07a071a9ed74be
paulafalves <paulafernandaalves@icloud.com>
2022-09-11 19:01:17 -0300

    feat: create products mock

---
+++ src/components/Cart/productsList.mock.js
@@ -0,0 +1,33 @@
+export const emptyCartProductsList = [
+  {
+    id: "1",
+    name: "Product A",
+    image: process.env.PUBLIC_URL + "/assets/a.jpg",
+    price: 20,
+    quantity: 0,
+  },
+  {
+    id: "2",
+    name: "Product B",
+    image: process.env.PUBLIC_URL + "/assets/b.jpg",
+    price: 10,
+    quantity: 0,
+  },
+];
+
+export const cartProductsList = [
+  {
+    id: "1",
+    name: "Product A",
+    image: process.env.PUBLIC_URL + "/assets/a.jpg",
+    price: 20,
+    quantity: 2,
+  },
+  {
+    id: "2",
+    name: "Product B",
+    image: process.env.PUBLIC_URL + "/assets/b.jpg",
+    price: 10,
+    quantity: 1,
+  },
+];
